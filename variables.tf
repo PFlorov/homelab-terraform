281f72beca817d07c2ee216b830f8d7d1c1f5c93
PFlorov <plamengerasimov99@gmail.com>
2026-03-19 17:33:15 +0000

    Add initial Terraform Cloudflare DNS configuration

---
+++ variables.tf
@@ -0,0 +1,10 @@
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare zone ID"
+  type        = string
+}
+
+variable "cloudflare_tunnel_target" {
+  description = "Cloudflare tunnel target hostname"
+  type        = string
+}
